2416a1c8d1a138bc9de035dca20063966b87d68f
JohnReesFujitsu <john.rees@nz.fujitsu.com>
2016-12-14 13:06:21 +1300

    Corrected tests

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -10,7 +10,7 @@ public class HelloMavenTest extends FunctionalTestCase {
 
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
-        runFlowAndExpect("mavenFlow", "Hello Maven FAIL");
+        runFlowAndExpect("mavenFlow", "Hello Maven");
     }
     
     @Test
